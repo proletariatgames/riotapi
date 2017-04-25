773445a989fa69f06cfa08abd68215d7d6c60539
djlennon <djlennon@gmail.com>
2017-04-25 16:47:39 -0400

    Basic API platform & client support - dep dependency configuration skeleton

---
+++ client.go
@@ -0,0 +1,99 @@
+package riotapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrAPIKeyRequired    = errors.New("Required APIKey is missing")
+	ErrUnknownPlatformId = errors.New("Unknown platform id")
+)
+
+// Version 3+ format, e.g. https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{name}
+const platformUrlBase = "https://{platform}.api.riotgames.com"
+
+// Version <3 format, e.g.
+const regionUrlBase = "https://{region}.api.riotgames.com/api"
+
+type Client struct {
+	client *http.Client
+	APIKey string
+	// TODO throttle rate limiting info
+	Summoner *SummonerMethod
+}
+
+// NewClient initializes and returns a riotapi Client struct
+func NewClient(httpClient *http.Client) *Client {
+	c := &Client{
+		client: httpClient,
+		APIKey: "",
+	}
+
+	c.Summoner = &SummonerMethod{client: c}
+
+	// TODO rate limiting info
+
+	return c
+}
+
+func (c *Client) get(basePath, relPath, platformId string, decoded interface{}) (*http.Response, error) {
+	if len(c.APIKey) == 0 {
+		return nil, ErrAPIKeyRequired
+	}
+
+	platform := GetPlatform(platformId)
+	if platform == nil {
+		return nil, ErrUnknownPlatformId
+	}
+
+	relURL, err := url.Parse(replaceTokens(relPath, platform))
+	if err != nil {
+		return nil, err
+	}
+
+	baseURL, _ := url.Parse(replaceTokens(basePath, platform))
+	combinedURL := baseURL.ResolveReference(relURL)
+
+	// Add the API Key
+	q := combinedURL.Query()
+	q.Set("api_key", c.APIKey)
+	combinedURL.RawQuery = q.Encode()
+
+	req, err := http.NewRequest("GET", combinedURL.String(), nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNotModified {
+		errBody := new(bytes.Buffer)
+		resp.Write(errBody)
+		return nil, errors.New("error, status code: " + strconv.Itoa(resp.StatusCode) + ", " + errBody.String())
+	}
+
+	if decoded != nil {
+		err = json.NewDecoder(resp.Body).Decode(decoded)
+	}
+
+	return resp, err
+}
+
+func replaceTokens(s string, platform *Platform) string {
+	s = strings.Replace(s, "{region}", platform.RegionId, -1)
+	s = strings.Replace(s, "{platform}", platform.Id, -1)
+	return s
+}
---
+++ platform.go
@@ -0,0 +1,38 @@
+package riotapi
+
+import (
+	"strings"
+)
+
+// Platform struct describes a Riot Platform
+type Platform struct {
+	Id       string // e.g. "NA1"
+	Name     string // e.g. "North America"
+	RegionId string // e.g. "NA"
+}
+
+var platforms = []Platform{
+	{"NA1", "North America", "NA"},
+	{"BR1", "Brazil", "BR"},
+	{"EUN1", "EU Nordic & East ", "EUNE"},
+	{"EUW1", "EU West", "EUW"},
+	{"JP1", "Japan", "JP"},
+	{"KR", "Korea", "KR"},
+	{"LA1", "Latin America North", "LAN"},
+	{"LA2", "Latin America South", "LAS"},
+	{"OC1", "Oceania", "OCE"},
+	{"PBE1", "PBE", "PBE"},
+	{"RU", "Russia", "RU"},
+	{"TR1", "Turkey", "TR"},
+}
+
+// GetPlatform returns the platform description for a given platform id (case-insensitive)
+func GetPlatform(id string) *Platform {
+	upperId := strings.ToUpper(id)
+	for _, p := range platforms {
+		if upperId == p.Id {
+			return &p
+		}
+	}
+	return nil
+}
